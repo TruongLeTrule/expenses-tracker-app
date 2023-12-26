9cf082797f9738f2f0185344b96d5b5815ca78be
Nguyễn Quang Vũ <21522801@gm.uit.edu.vn>
2023-12-26 22:16:56 +0700

    Cập nhật thanh progress sau khi expense được tạo

--- screens/BudgetsScreen.js
+++ screens/BudgetsScreen.js
@@ -1,23 +1,29 @@
-import { View, Text, StyleSheet, TouchableOpacity, Modal, FlatList, ActivityIndicator } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, FlatList, ActivityIndicator, Alert } from "react-native";
+import Modal  from "react-native-modal"
 import { useEffect } from "react";
 import BottomSheet from "../components/BudgetsScreen/BottomSheet";
 import Budgets from "../components/BudgetsScreen/Budgets";
-import { getDocs, collection } from "firebase/firestore";
+import { getDocs, collection, query, where } from "firebase/firestore";
 import { db } from "../firebase";
 import useStore from "../data/useStore";
 import WhiteBox from "../components/WalletsScreen/WhiteBox";
+import { commafy } from "../components/formatCurrency";
+import { Ionicons } from "@expo/vector-icons";
 export default function BudgetsScreen() {
   const data = useStore((state) => state.data);
   const modalVisible = useStore((state) => state.modalVisible);
   const time = useStore((state) => state.time);
   const isLoading = useStore((state) => state.isLoading);
   const uid = useStore((state) => state.uid);
+  const allExpenses = useStore((state) => state.allExpenses);
 
   {/* Fetch data from Firestore */ }
   const fetchDataFromFirestore = async () => {
     try {
       const data = [];
-      const querySnapshot = await getDocs(collection(db, "Budget"));
+      const budgetRef = collection(db, "Budget");
+      const q = query(budgetRef, where("uid", "==", uid));
+      const querySnapshot = await getDocs(q);
       querySnapshot.forEach((doc) => {
         const docData = doc.data();
         data.push({
@@ -35,7 +41,7 @@ export default function BudgetsScreen() {
         const orderB = time.indexOf(b.timerange?.type);
         return orderA - orderB;
       });
-
+      console.log('Budgets fetched')
       return data;
     } catch (error) {
       console.error("Error fetching data from Firestore:", error);
@@ -53,11 +59,22 @@ export default function BudgetsScreen() {
     fetchData();
   }, []);
 
+    //Get expense
+  const getExpense = (category) => {
+    let totalExpense = 0;
+    allExpenses.map((expense) => {
+      if (category === expense.category) {
+        totalExpense += expense.value;
+      }
+    });
+    return totalExpense;
+  };
+
   {/* Loading screen */ }
   if (isLoading) {
     return (
       <View className="flex-1 justify-center items-center">
-        <ActivityIndicator size={"large"} />
+        <ActivityIndicator size={"large"} color={'#4cb050'} />
       </View>
     );
   }
@@ -79,18 +96,21 @@ export default function BudgetsScreen() {
       <View style={styles.header}>
         <Text style={styles.text}>Budgets</Text>
       </View>
-      {data ? (
         <View style={styles.container}>
           <FlatList
             data={time}
             keyExtractor={(item) => item}
             renderItem={({ item }) => {
               const budgetsForTimeRange = data.filter(budget => budget.timerange?.type === item);
+              const totalBudget = (value, category) => {
+                let totalBudget = 0;
+                totalBudget = value - getExpense(category);
+                return totalBudget;
+              }
                 if (budgetsForTimeRange.length > 0) {
                   return (
                     <WhiteBox mt={"mt-4"}>
                       <View style={styles.boxContainer}>
-                        <View style={styles.box}>
                           <View>
                             <Text style={styles.boxText}>{item}</Text>
                             <View style={styles.line} />
@@ -99,21 +119,55 @@ export default function BudgetsScreen() {
                             <Budgets
                               key={budget.name}
                               name={budget.name}
-                              value={budget.value}
+                              value={commafy(totalBudget(budget.value, budget.category))}
                               category={budget.category}
+                              onPress={() => {Alert.alert(budget.name,`budget value: ${budget.value}đ`)}}
+                              progress={
+                                totalBudget(budget.value, budget.category) < 0 
+                                ? 1 - (getExpense(budget.category) / 1000000) 
+                                : getExpense(budget.category) / budget.value
+                              }
+                              color={totalBudget(budget.value, budget.category) < 0 ? '#eb3700': '#4cb050'}
+                              unfilledColor={totalBudget(budget.value, budget.category) < 0 ? '#eb3700' : '#f2f2f2'} //Budget exceeded
                             />
                           ))}
-                        </View>
                       </View>
                     </WhiteBox>
-                  )}}}
-            ListFooterComponent={
-              <TouchableOpacity style={styles.button} onPress={() => {useStore.setState({ modalVisible: true })}}>
-                <Text style={styles.text}>Add Budget</Text>
-              </TouchableOpacity>
-            }
+                  )}
+                  
+                  else{
+                    return null;
+                  }
+                  }}
+                ListFooterComponent={
+                  (data && data.length !== 0) && (
+                  <View className="items-center mt-8">
+                    <TouchableOpacity
+                      className="w-70 h-14 rounded-full bg-primary flex-row items-center p-2"
+                      onPress={() => {useStore.setState({ modalVisible: true })}}
+                    >
+                      <Ionicons name={"add"} size={30} color={"#fff"} />
+                      <Text className="font-bold text-xl text-[#fff] ml-2">Add Budget</Text>
+                    </TouchableOpacity>
+                  </View>
+                  )
+                }
           />
+          {(!data || data.length === 0) && (
+            <View className=' flex-1'>
+              <Text className='text-lg text-center mb-5'>You have not created any budget yet</Text>
+              <TouchableOpacity style={styles.button} onPress={() => { useStore.setState({ modalVisible: true })}}>
+                <Ionicons name={"add"} size={30} color={"#fff"} />
+                <Text className="font-bold text-xl text-[#fff] ml-2">Add Budget</Text>
+              </TouchableOpacity>
+            </View>
+          )}
           <Modal
+            swipeDirection="down"
+            onBackdropPress={() => { useStore.setState({ modalVisible: false })}}
+            onSwipeComplete={() => { useStore.setState({ modalVisible: false })}}
+            style={styles.modalContainer}
+            className="flex-1 m-0 justify-end"
             animationType="slide"
             transparent={true}
             visible={modalVisible}
@@ -124,16 +178,6 @@ export default function BudgetsScreen() {
             <BottomSheet onPress={reset} />
           </Modal>
         </View>
-      ) : (
-        <View style={styles.container}>
-          <Text style={styles.text}>No budget yet</Text>
-          <TouchableOpacity style={styles.button} onPress={() => {useStore.setState({ modalVisible: true })}}>
-            <Text style={styles.text}>Add Budget</Text>
-          </TouchableOpacity>
-        </View>
-      )
-      }
-
     </View>
   );
 }
@@ -158,7 +202,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
   },
   text: {
-    fontSize: 20,
+    fontSize: 25,
     fontWeight: "bold",
     color: "white",
     textAlign: "center",
@@ -185,9 +229,6 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     justifyContent: "center",
     paddingVertical: 10,
-  },
-  box: {
-
   },
   line: {
     borderWidth: 0.2,
