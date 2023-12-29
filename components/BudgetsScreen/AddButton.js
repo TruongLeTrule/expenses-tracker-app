416c8e180e32e0fce8f9ffd667dee1764b1fbbe5
Nguyễn Quang Vũ <21522801@gm.uit.edu.vn>
2023-12-29 11:44:58 +0700

    Add files via upload

---
+++ components/BudgetsScreen/AddButton.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+const AddButton = ({ onPress }) => (
+    <TouchableOpacity
+        className="w-70 h-14 rounded-full bg-primary flex-row items-center p-2"
+        onPress={onPress}
+    >
+        <Ionicons name={"add"} size={30} color={"#fff"} />
+        <Text className="font-bold text-xl text-[#fff] ml-2">Add Budget</Text>
+    </TouchableOpacity>
+    );
+export default AddButton;
