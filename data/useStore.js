9005f3bd6b6569a34ccfb3c12ffa622d4d6d8ff7
Pham Minh Triet <vodanhtinh203@gmail.com>
2024-01-05 22:05:46 +0700

    delete comments

--- data/useStore.js
+++ data/useStore.js
@@ -1,334 +1,236 @@
 import { create } from "zustand";
 
 const useStore = create((set) => ({
-  // User info
-  uid: null,
-  setUID: (data) => {
-    set((state) => ({ uid: data }));
-  },
+	// User info
+	uid: null,
+	setUID: (data) => {
+		set((state) => ({ uid: data }));
+	},
 
-  // All expenses and income array
-  allExpensesAndIncomes: null,
-  setAllExpensesAndIncome: (data) => {
-    set((state) => ({ allExpensesAndIncomes: data }));
-  },
+	// All expenses and income array
+	allExpensesAndIncomes: null,
+	setAllExpensesAndIncome: (data) => {
+		set((state) => ({ allExpensesAndIncomes: data }));
+	},
 
-  // Expenses and Income grouped by date
-  sortDateExpensesAndIncome: null,
-  setSortDateExpensesAndIncome: (data) => {
-    // Group list by date which date is title and data is a group of expense and income
-    const groupByDate = (list) => {
-      if (list.length) {
-        // Set last date to the last date of list
-        let lastDate = new Date(list[0].date.seconds * 1000);
-        let smallList = [list[0]];
-        let result = [];
+	// Expenses and Income grouped by date
+	sortDateExpensesAndIncome: null,
+	setSortDateExpensesAndIncome: (data) => {
+		// Group list by date which date is title and data is a group of expense and income
+		const groupByDate = (list) => {
+			if (list.length) {
+				// Set last date to the last date of list
+				let lastDate = new Date(list[0].date.seconds * 1000);
+				let smallList = [list[0]];
+				let result = [];
 
-        for (let i = 0; i < list.length; i++) {
-          const itemDate = new Date(list[i].date.seconds * 1000);
+				for (let i = 0; i < list.length; i++) {
+					const itemDate = new Date(list[i].date.seconds * 1000);
 
-          // Push small list into result if current item has different date
-          if (lastDate.toDateString() !== itemDate.toDateString()) {
-            result.push({
-              title: lastDate,
-              data: [...smallList],
-            });
+					// Push small list into result if current item has different date
+					if (lastDate.toDateString() !== itemDate.toDateString()) {
+						result.push({
+							title: lastDate,
+							data: [...smallList],
+						});
 
-            // Set last date to current item date and empty the array
-            lastDate = itemDate;
-            smallList = [];
-          }
+						// Set last date to current item date and empty the array
+						lastDate = itemDate;
+						smallList = [];
+					}
 
-          // Don't push into small list if this is the first element,
-          // cause is already pushed in the declaration
-          if (i !== 0) {
-            smallList.push(list[i]);
-          }
+					// Don't push into small list if this is the first element,
+					// cause is already pushed in the declaration
+					if (i !== 0) {
+						smallList.push(list[i]);
+					}
 
-          // Push small list into result if this is the last element
-          if (i === list.length - 1) {
-            result.push({
-              title: lastDate,
-              data: [...smallList],
-            });
-          }
-        }
+					// Push small list into result if this is the last element
+					if (i === list.length - 1) {
+						result.push({
+							title: lastDate,
+							data: [...smallList],
+						});
+					}
+				}
 
-        return result;
-      }
-    };
-    set((state) => ({ sortDateExpensesAndIncome: groupByDate(data) }));
-  },
+				return result;
+			}
+		};
+		set((state) => ({ sortDateExpensesAndIncome: groupByDate(data) }));
+	},
 
-  // All expenses array
-  allExpenses: null,
-  setAllExpenses: (data) => {
-    set((state) => ({ allExpenses: data }));
-  },
+	// All expenses array
+	allExpenses: null,
+	setAllExpenses: (data) => {
+		set((state) => ({ allExpenses: data }));
+	},
 
-  // Expenses grouped by date
-  // sortDateExpenses: null,
-  // setSortDateExpenses: (data) => {
-  //   // Group list by date which date is title and data is a group of expense and income
-  //   const groupByDate = (list) => {
-  //     if (list) {
-  //       if (list.length) {
-  //         // Set last date to the last date of list
-  //         let lastDate = new Date(list[0].date.seconds * 1000);
-  //         let smallList = [list[0]];
-  //         let result = [];
+	// All incomes array
+	allIncomes: null,
+	setAllIncomes: (data) => {
+		set((state) => ({ allIncomes: data }));
+	},
 
-  //         for (let i = 0; i < list.length; i++) {
-  //           const itemDate = new Date(list[i].date.seconds * 1000);
+	// Filtered transactions list
+	filteredList: null,
+	setFilteredList: (data) => {
+		set((state) => ({ filteredList: data }));
+	},
 
-  //           // Push small list into result if current item has different date
-  //           if (lastDate.toDateString() !== itemDate.toDateString()) {
-  //             result.push({
-  //               title: lastDate,
-  //               data: [...smallList],
-  //             });
+	// Render list which combine all expense and income together
+	renderList: null,
+	setRenderList: (data) => {
+		// Group list by date which date is title and data is a group of expense and income
+		const groupByDate = (list) => {
+			if (list) {
+				if (list.length) {
+					// Set last date to the last date of list
+					let lastDate = new Date(list[0].date.seconds * 1000);
+					let smallList = [list[0]];
+					let result = [];
 
-  //             // Set last date to current item date and empty the array
-  //             lastDate = itemDate;
-  //             smallList = [];
-  //           }
+					for (let i = 0; i < list.length; i++) {
+						const itemDate = new Date(list[i].date.seconds * 1000);
 
-  //           // Don't push into small list if this is the first element,
-  //           // cause is already pushed in the declaration
-  //           if (i !== 0) {
-  //             smallList.push(list[i]);
-  //           }
+						// Push small list into result if current item has different date
+						if (lastDate.toDateString() !== itemDate.toDateString()) {
+							result.push({
+								title: lastDate,
+								data: [...smallList],
+							});
 
-  //           // Push small list into result if this is the last element
-  //           if (i === list.length - 1) {
-  //             result.push({
-  //               title: lastDate,
-  //               data: [...smallList],
-  //             });
-  //           }
-  //         }
+							// Set last date to current item date and empty the array
+							lastDate = itemDate;
+							smallList = [];
+						}
 
-  //         return result;
-  //       }
-  //     }
-  //   };
-  //   set((state) => ({ sortDateExpenses: groupByDate(data) }));
-  // },
+						// Don't push into small list if this is the first element,
+						// cause is already pushed in the declaration
+						if (i !== 0) {
+							smallList.push(list[i]);
+						}
 
-  // // Incomes grouped by date
-  // sortDateIncomes: null,
-  // setSortDateIncomes: (data) => {
-  //   // Group list by date which date is title and data is a group of expense and income
-  //   const groupByDate = (list) => {
-  //     if (list) {
-  //       if (list.length) {
-  //         // Set last date to the last date of list
-  //         let lastDate = new Date(list[0].date.seconds * 1000);
-  //         let smallList = [list[0]];
-  //         let result = [];
+						// Push small list into result if this is the last element
+						if (i === list.length - 1) {
+							result.push({
+								title: lastDate,
+								data: [...smallList],
+							});
+						}
+					}
 
-  //         for (let i = 0; i < list.length; i++) {
-  //           const itemDate = new Date(list[i].date.seconds * 1000);
+					return result;
+				}
+			}
+		};
+		set((state) => ({ renderList: groupByDate(data) }));
+	},
 
-  //           // Push small list into result if current item has different date
-  //           if (lastDate.toDateString() !== itemDate.toDateString()) {
-  //             result.push({
-  //               title: lastDate,
-  //               data: [...smallList],
-  //             });
+	// Total value of expenses,
+	totalExpense: 0,
+	setTotalExpense: (data) => {
+		// Get total expense, data is an array of expenses
+		const getTotalExpense = (expenses) =>
+			expenses.reduce((result, expense) => result + expense.value, 0);
+		set((state) => ({ totalExpense: getTotalExpense(data) }));
+	},
 
-  //             // Set last date to current item date and empty the array
-  //             lastDate = itemDate;
-  //             smallList = [];
-  //           }
+	// Total value of incomes, data is an array of incomes
+	totalIncome: 0,
+	setTotalIncome: (data) => {
+		// Get total income
+		const getTotalIncome = (incomes) =>
+			incomes.reduce((result, income) => result + income.value, 0);
+		set((state) => ({ totalIncome: getTotalIncome(data) }));
+	},
 
-  //           // Don't push into small list if this is the first element,
-  //           // cause is already pushed in the declaration
-  //           if (i !== 0) {
-  //             smallList.push(list[i]);
-  //           }
+	// Total value calculation of income and expense
+	total: 0,
+	setTotal: (data) => {
+		set((state) => ({ total: data }));
+	},
 
-  //           // Push small list into result if this is the last element
-  //           if (i === list.length - 1) {
-  //             result.push({
-  //               title: lastDate,
-  //               data: [...smallList],
-  //             });
-  //           }
-  //         }
+	// Modal state
+	editModalVisible: false,
+	toggleEditModalVisible: () => {
+		set((state) => ({ editModalVisible: !state.editModalVisible }));
+	},
+	categoryModalVisible: false,
+	toggleCategoryModalVisible: () => {
+		set((state) => ({ categoryModalVisible: !state.categoryModalVisible }));
+	},
+	addModalVisible: false,
+	toggleAddModalVisible: () => {
+		set((state) => ({ addModalVisible: !state.addModalVisible }));
+	},
 
-  //         return result;
-  //       }
-  //     }
-  //   };
-  //   set((state) => ({ sortDateIncomes: groupByDate(data) }));
-  // },
+	monthYearModalVisible: false,
+	toggleMonthYearModalVisible: () => {
+		set((state) => ({ monthYearModalVisible: !state.monthYearModalVisible }));
+	},
 
-  // All incomes array
-  allIncomes: null,
-  setAllIncomes: (data) => {
-    set((state) => ({ allIncomes: data }));
-  },
+	// Loading state
+	isLoadingInWalletScreen: false,
+	setIsLoadingInWalletScreen: (data) => {
+		set((state) => ({ isLoadingInWalletScreen: data }));
+	},
 
-  // Filtered transactions list
-  filteredList: null,
-  setFilteredList: (data) => {
-    set((state) => ({ filteredList: data }));
-  },
+	// Editing transaction
+	editingTransaction: null,
+	setEditingTransaction: (transaction) => {
+		set((state) => ({ editingTransaction: transaction }));
+	},
 
-  // Render list which combine all expense and income together
-  renderList: null,
-  setRenderList: (data) => {
-    // Group list by date which date is title and data is a group of expense and income
-    const groupByDate = (list) => {
-      if (list) {
-        if (list.length) {
-          // Set last date to the last date of list
-          let lastDate = new Date(list[0].date.seconds * 1000);
-          let smallList = [list[0]];
-          let result = [];
+	//Budget state
+	editMode: false,
+	setEditMode: (editMode) => set({ editMode }),
 
-          for (let i = 0; i < list.length; i++) {
-            const itemDate = new Date(list[i].date.seconds * 1000);
+	budgetId: "",
+	setBudgetId: (budgetId) => set({ budgetId }),
 
-            // Push small list into result if current item has different date
-            if (lastDate.toDateString() !== itemDate.toDateString()) {
-              result.push({
-                title: lastDate,
-                data: [...smallList],
-              });
+	data: [],
+	setData: (data) => set({ data }),
 
-              // Set last date to current item date and empty the array
-              lastDate = itemDate;
-              smallList = [];
-            }
+	budgetName: "",
+	setBudgetName: (budgetName) => set({ budgetName }),
 
-            // Don't push into small list if this is the first element,
-            // cause is already pushed in the declaration
-            if (i !== 0) {
-              smallList.push(list[i]);
-            }
+	budgetAmount: "",
+	setBudgetAmount: (budgetAmount) => set({ budgetAmount }),
 
-            // Push small list into result if this is the last element
-            if (i === list.length - 1) {
-              result.push({
-                title: lastDate,
-                data: [...smallList],
-              });
-            }
-          }
+	budgetTime: "Time Range",
+	setBudgetTime: (budgetTime) => set({ budgetTime }),
 
-          return result;
-        }
-      }
-    };
-    set((state) => ({ renderList: groupByDate(data) }));
-  },
+	modalVisible: false,
+	setModalVisible: (modalVisible) => set({ modalVisible }),
 
-  // Total value of expenses,
-  totalExpense: 0,
-  setTotalExpense: (data) => {
-    // Get total expense, data is an array of expenses
-    const getTotalExpense = (expenses) =>
-      expenses.reduce((result, expense) => result + expense.value, 0);
-    set((state) => ({ totalExpense: getTotalExpense(data) }));
-  },
+	budgetCategory: "Categories",
+	setBudgetCategory: (budgetCategory) => set({ budgetCategory }),
 
-  // Total value of incomes, data is an array of incomes
-  totalIncome: 0,
-  setTotalIncome: (data) => {
-    // Get total income
-    const getTotalIncome = (incomes) =>
-      incomes.reduce((result, income) => result + income.value, 0);
-    set((state) => ({ totalIncome: getTotalIncome(data) }));
-  },
+	isLoading: true,
+	setIsLoading: (isLoading) => set({ isLoading }),
 
-  // Total value calculation of income and expense
-  total: 0,
-  setTotal: (data) => {
-    set((state) => ({ total: data }));
-  },
+	time: ["Weekly", "Monthly", "Quarterly", "Half Yearly", "Yearly"],
 
-  // Modal state
-  editModalVisible: false,
-  toggleEditModalVisible: () => {
-    set((state) => ({ editModalVisible: !state.editModalVisible }));
-  },
-  categoryModalVisible: false,
-  toggleCategoryModalVisible: () => {
-    set((state) => ({ categoryModalVisible: !state.categoryModalVisible }));
-  },
-  addModalVisible: false,
-  toggleAddModalVisible: () => {
-    set((state) => ({ addModalVisible: !state.addModalVisible }));
-  },
+	reportFilterVisible: true,
+	setReportFilterVisible: (data) => {
+		set(() => ({ reportFilterVisible: data }));
+	},
 
-  monthYearModalVisible: false,
-  toggleMonthYearModalVisible: () => {
-    set((state) => ({ monthYearModalVisible: !state.monthYearModalVisible }));
-  },
+	selectedDate: {},
+	setSelectedDate: (data) => {
+		set(() => ({ selectedDate: data }));
+	},
 
-  // Loading state
-  isLoadingInWalletScreen: false,
-  setIsLoadingInWalletScreen: (data) => {
-    set((state) => ({ isLoadingInWalletScreen: data }));
-  },
+	userInfo: null,
+	setUserInfo: (data) => {
+		set(() => ({ userInfo: data }));
+	},
 
-  // Editing transaction
-  editingTransaction: null,
-  setEditingTransaction: (transaction) => {
-    set((state) => ({ editingTransaction: transaction }));
-  },
-
-  //Budget state
-  editMode: false,
-  setEditMode: (editMode) => set({ editMode }),
-
-  budgetId: "",
-  setBudgetId: (budgetId) => set({ budgetId }),
-
-  data: [],
-  setData: (data) => set({ data }),
-
-  budgetName: "",
-  setBudgetName: (budgetName) => set({ budgetName }),
-
-  budgetAmount: "",
-  setBudgetAmount: (budgetAmount) => set({ budgetAmount }),
-
-  budgetTime: "Time Range",
-  setBudgetTime: (budgetTime) => set({ budgetTime }),
-
-  modalVisible: false,
-  setModalVisible: (modalVisible) => set({ modalVisible }),
-
-  budgetCategory: "Categories",
-  setBudgetCategory: (budgetCategory) => set({ budgetCategory }),
-
-  isLoading: true,
-  setIsLoading: (isLoading) => set({ isLoading }),
-
-  time: ["Weekly", "Monthly", "Quarterly", "Half Yearly", "Yearly"],
-
-  reportFilterVisible: true,
-  setReportFilterVisible: (data) => {
-    set(() => ({ reportFilterVisible: data }));
-  },
-
-  selectedDate: {},
-  setSelectedDate: (data) => {
-    set(() => ({ selectedDate: data }));
-  },
-
-  userInfo: null,
-  setUserInfo: (data) => {
-    set(() => ({ userInfo: data }));
-  },
-
-  avaURI: null,
-  setAvaURI: (data) => {
-    set(() => ({ avaURI: data }));
-  },
+	avaURI: null,
+	setAvaURI: (data) => {
+		set(() => ({ avaURI: data }));
+	},
 }));
 
 export default useStore;
