f97bc8af1116fbf40509adde4ad0f4d3a44a5bb1
Pham Minh Triet <vodanhtinh203@gmail.com>
2024-01-05 22:28:18 +0700

    small update wallet screen

--- components/WalletsScreen/Header.js
+++ components/WalletsScreen/Header.js
@@ -11,80 +11,92 @@ import FilterByCategory from "./FilterByCategory";
 import { commafy } from "../formatCurrency";
 
 const Header = ({ total }) => {
-  const setFilteredList = useStore((state) => state.setFilteredList);
+	const setFilteredList = useStore((state) => state.setFilteredList);
 
-  const [filterModalVisible, setFilterModalVisible] = useState(false);
+	const [filterModalVisible, setFilterModalVisible] = useState(false);
 
-  return (
-    <View className="bg-primary h-40 px-4 justify-center">
-      <WhiteBox>
-        <View className="flex-row justify-between">
-          {/* Wallet group */}
-          <View>
-            <View className="flex-row items-center gap-3">
-              <View className="bg-dark-green rounded-full h-11 w-11 flex items-center justify-center">
-                <Ionicons name="cash" size={24} color={"#fff"} />
-              </View>
-              <Text className="text-2xl text-grey-text font-normal">Cash</Text>
-            </View>
-            <Text
-              className="text-4xl font-bold mt-2"
-              style={{ color: total < 0 ? "#f04433" : "#4cb050" }}
-              numberOfLines={1}
-            >
-              {commafy(total)}₫
-            </Text>
-          </View>
+	return (
+		<View className="bg-primary h-40 px-4 justify-center">
+			<WhiteBox style={{ position: "relative" }}>
+				<View className="flex-row justify-between">
+					{/* Wallet group */}
+					<View>
+						<View className="flex-row items-center gap-3">
+							<View className="bg-dark-green rounded-full h-11 w-11 flex items-center justify-center">
+								<Ionicons
+									name="cash"
+									size={24}
+									color={"#fff"}
+								/>
+							</View>
+							<Text className="text-2xl text-grey-text font-normal">Cash</Text>
+						</View>
+						<Text
+							className="text-4xl font-bold mt-2"
+							style={{ color: total < 0 ? "#f04433" : "#4cb050" }}
+							numberOfLines={1}>
+							{commafy(total)}₫
+						</Text>
+					</View>
 
-          {/* Filter button */}
-          <Pressable
-            className="rounded-full h-11 w-11 flex items-center justify-center"
-            onPress={() => setFilterModalVisible(true)}
-          >
-            <Ionicons name="filter" size={40} color={"#4cb050"} />
-          </Pressable>
-        </View>
-      </WhiteBox>
+					{/* Filter button */}
+					<Pressable
+						className="rounded-full h-11 w-11 flex items-center justify-center"
+						onPress={() => setFilterModalVisible(true)}
+						style={{ position: "absolute", top: 0, right: 0 }}>
+						<Ionicons
+							name="filter"
+							size={40}
+							color={"#4cb050"}
+						/>
+					</Pressable>
+				</View>
+			</WhiteBox>
 
-      {/* Filter modal */}
-      <Modal
-        isVisible={filterModalVisible}
-        onBackdropPress={() => setFilterModalVisible(false)}
-        animationIn={"fadeIn"}
-        animationOut={"fadeOut"}
-        className="flex-1 justify-center items-center"
-      >
-        <View className="bg-[#fff] justify-center items-center w-4/6 rounded-lg p-6">
-          {/* Title */}
-          <View className="flex-row justify-center items-center">
-            <Text className="text-2xl text-primary font-bold mr-2">Filter</Text>
-            <Ionicons name="filter" size={32} color={"#4cb050"} />
-          </View>
+			{/* Filter modal */}
+			<Modal
+				isVisible={filterModalVisible}
+				onBackdropPress={() => setFilterModalVisible(false)}
+				animationIn={"fadeIn"}
+				animationOut={"fadeOut"}
+				className="flex-1 justify-center items-center">
+				<View className="bg-[#fff] justify-center items-center w-4/6 rounded-lg p-6">
+					{/* Title */}
+					<View className="flex-row justify-center items-center">
+						<Text className="text-2xl text-primary font-bold mr-2">Filter</Text>
+						<Ionicons
+							name="filter"
+							size={32}
+							color={"#4cb050"}
+						/>
+					</View>
 
-          {/* Settings group */}
-          <View className="mt-6">
-            {/* Filter by date */}
-            <FilterByDate />
+					{/* Settings group */}
+					<View className="mt-6">
+						{/* Filter by date */}
+						<FilterByDate />
 
-            {/* Filter by category */}
-            <FilterByCategory />
+						{/* Filter by category */}
+						<FilterByCategory />
 
-            {/* Show all */}
-            <TouchableOpacity
-              className="flex-row justify-center items-center mt-2"
-              onPress={() => {
-                setFilteredList(null);
-                setFilterModalVisible(false);
-              }}
-            >
-              <Text className="text-lg font-bold mr-2">Show all</Text>
-              <Ionicons name="eye" size={25} />
-            </TouchableOpacity>
-          </View>
-        </View>
-      </Modal>
-    </View>
-  );
+						{/* Show all */}
+						<TouchableOpacity
+							className="flex-row justify-center items-center mt-2"
+							onPress={() => {
+								setFilteredList(null);
+								setFilterModalVisible(false);
+							}}>
+							<Text className="text-lg font-bold mr-2">Show all</Text>
+							<Ionicons
+								name="eye"
+								size={25}
+							/>
+						</TouchableOpacity>
+					</View>
+				</View>
+			</Modal>
+		</View>
+	);
 };
 
 export default Header;
--- screens/WalletsScreen.js
+++ screens/WalletsScreen.js
@@ -1,18 +1,18 @@
 import {
-  View,
-  Text,
-  StatusBar,
-  ActivityIndicator,
-  TouchableOpacity,
+	View,
+	Text,
+	StatusBar,
+	ActivityIndicator,
+	TouchableOpacity,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { useEffect, useState } from "react";
 import { FlatList } from "react-native-gesture-handler";
 import Animated, {
-  useSharedValue,
-  withTiming,
-  Easing,
-  useAnimatedStyle,
+	useSharedValue,
+	withTiming,
+	Easing,
+	useAnimatedStyle,
 } from "react-native-reanimated";
 
 import useFetch from "../data/fetchData";
@@ -26,237 +26,226 @@ import Overall from "../components/WalletsScreen/Overall";
 import EditModal from "../components/WalletsScreen/EditModal";
 
 export default function Wallets() {
-  const { getAllExpenses, getAllIncomes } = useFetch();
+	const { getAllExpenses, getAllIncomes } = useFetch();
 
-  const isLoadingInWalletScreen = useStore(
-    (state) => state.isLoadingInWalletScreen
-  );
-  const setIsLoadingInWalletScreen = useStore(
-    (state) => state.setIsLoadingInWalletScreen
-  );
-  const allExpenses = useStore((state) => state.allExpenses);
-  const allIncomes = useStore((state) => state.allIncomes);
-  const filteredList = useStore((state) => state.filteredList);
-  const renderList = useStore((state) => state.renderList);
-  const setRenderList = useStore((state) => state.setRenderList);
-  const totalExpense = useStore((state) => state.totalExpense);
-  const setTotalExpense = useStore((state) => state.setTotalExpense);
-  const totalIncome = useStore((state) => state.totalIncome);
-  const setTotalIncome = useStore((state) => state.setTotalIncome);
-  const total = useStore((state) => state.total);
-  const setTotal = useStore((state) => state.setTotal);
-  const uid = useStore((state) => state.uid);
-  // const setSortDateExpenses = useStore((state) => state.setSortDateExpenses);
-  // const setSortDateIncomes = useStore((state) => state.setSortDateIncomes);
-  const setSortDateExpensesAndIncome = useStore(
-    (state) => state.setSortDateExpensesAndIncome
-  );
+	const isLoadingInWalletScreen = useStore(
+		(state) => state.isLoadingInWalletScreen,
+	);
+	const setIsLoadingInWalletScreen = useStore(
+		(state) => state.setIsLoadingInWalletScreen,
+	);
+	const allExpenses = useStore((state) => state.allExpenses);
+	const allIncomes = useStore((state) => state.allIncomes);
+	const filteredList = useStore((state) => state.filteredList);
+	const renderList = useStore((state) => state.renderList);
+	const setRenderList = useStore((state) => state.setRenderList);
+	const totalExpense = useStore((state) => state.totalExpense);
+	const setTotalExpense = useStore((state) => state.setTotalExpense);
+	const totalIncome = useStore((state) => state.totalIncome);
+	const setTotalIncome = useStore((state) => state.setTotalIncome);
+	const total = useStore((state) => state.total);
+	const setTotal = useStore((state) => state.setTotal);
+	const uid = useStore((state) => state.uid);
+	// const setSortDateExpenses = useStore((state) => state.setSortDateExpenses);
+	// const setSortDateIncomes = useStore((state) => state.setSortDateIncomes);
+	const setSortDateExpensesAndIncome = useStore(
+		(state) => state.setSortDateExpensesAndIncome,
+	);
 
-  // Toggle list visible feature
-  const [chevronType, setChevronType] = useState("chevron-up");
-  const height = useSharedValue(100);
-  const handleListVisible = () => {
-    if (height.value === 100) {
-      height.value = withTiming(height.value - 100, {
-        duration: 340,
-        easing: Easing.inOut(Easing.quad),
-      });
-      setChevronType("chevron-down");
-    }
-    if (height.value === 0) {
-      height.value = withTiming(height.value + 100, {
-        duration: 340,
-        easing: Easing.inOut(Easing.quad),
-      });
-      setChevronType("chevron-up");
-    }
-  };
-  const animatedStyle = useAnimatedStyle(() => ({
-    height: `${height.value}%`,
-  }));
+	// Toggle list visible feature
+	const [chevronType, setChevronType] = useState("chevron-up");
+	const height = useSharedValue(100);
+	const handleListVisible = () => {
+		if (height.value === 100) {
+			height.value = withTiming(height.value - 100, {
+				duration: 340,
+				easing: Easing.inOut(Easing.quad),
+			});
+			setChevronType("chevron-down");
+		}
+		if (height.value === 0) {
+			height.value = withTiming(height.value + 100, {
+				duration: 340,
+				easing: Easing.inOut(Easing.quad),
+			});
+			setChevronType("chevron-up");
+		}
+	};
+	const animatedStyle = useAnimatedStyle(() => ({
+		height: `${height.value}%`,
+	}));
 
-  // Get data from DB, then set loading state
-  const getDataFromDB = async () => {
-    setIsLoadingInWalletScreen(true);
-    await getAllExpenses(uid);
-    await getAllIncomes(uid);
-    setIsLoadingInWalletScreen(false);
-  };
+	// Get data from DB, then set loading state
+	const getDataFromDB = async () => {
+		setIsLoadingInWalletScreen(true);
+		await getAllExpenses(uid);
+		await getAllIncomes(uid);
+		setIsLoadingInWalletScreen(false);
+	};
 
-  // If the is no expense or income in local, get them from db
-  useEffect(() => {
-    if (!allExpenses && !allIncomes) {
-      getDataFromDB();
-    }
-  }, []);
+	// If the is no expense or income in local, get them from db
+	useEffect(() => {
+		if (!allExpenses && !allIncomes) {
+			getDataFromDB();
+		}
+	}, []);
 
-  // Set render array again if income or expense has been changed
-  // or there is filter feature enabled
-  useEffect(() => {
-    if (filteredList) {
-      // Sort list every rendering
-      filteredList.sort(function (a, b) {
-        // Convert the date strings to Date objects
-        let dateA = a.date.seconds;
-        let dateB = b.date.seconds;
+	// Set render array again if income or expense has been changed
+	// or there is filter feature enabled
+	useEffect(() => {
+		if (filteredList) {
+			// Sort list every rendering
+			filteredList.sort(function (a, b) {
+				// Convert the date strings to Date objects
+				let dateA = a.date.seconds;
+				let dateB = b.date.seconds;
 
-        // Subtract the dates to get a value that is either negative, positive, or zero
-        return dateB - dateA;
-      });
-      setRenderList(filteredList);
-    } else if (allExpenses || allIncomes) {
-      let renderArr;
-      if (!allExpenses && allIncomes) {
-        renderArr = [...allIncomes];
-      } else if (allExpenses && !allIncomes) {
-        renderArr = [...allExpenses];
-      } else {
-        renderArr = [...allExpenses, ...allIncomes];
-      }
-      // Sort list every rendering
-      renderArr.sort(function (a, b) {
-        // Convert the date strings to Date objects
-        let dateA = a.date.seconds;
-        let dateB = b.date.seconds;
+				// Subtract the dates to get a value that is either negative, positive, or zero
+				return dateB - dateA;
+			});
+			setRenderList(filteredList);
+		} else if (allExpenses || allIncomes) {
+			let renderArr;
+			if (!allExpenses && allIncomes) {
+				renderArr = [...allIncomes];
+			} else if (allExpenses && !allIncomes) {
+				renderArr = [...allExpenses];
+			} else {
+				renderArr = [...allExpenses, ...allIncomes];
+			}
+			// Sort list every rendering
+			renderArr.sort(function (a, b) {
+				// Convert the date strings to Date objects
+				let dateA = a.date.seconds;
+				let dateB = b.date.seconds;
 
-        // Subtract the dates to get a value that is either negative, positive, or zero
-        return dateB - dateA;
-      });
-      setRenderList(renderArr);
-    }
-  }, [allExpenses, allIncomes, filteredList]);
+				// Subtract the dates to get a value that is either negative, positive, or zero
+				return dateB - dateA;
+			});
+			setRenderList(renderArr);
+		}
+	}, [allExpenses, allIncomes, filteredList]);
 
-  // Set total expense when all expenses have been changed
-  useEffect(() => {
-    if (allExpenses) {
-      setTotalExpense(allExpenses);
-      // setSortDateExpenses(
-      //   allExpenses.sort(function (a, b) {
-      //     // Convert the date strings to Date objects
-      //     let dateA = a.date.seconds;
-      //     let dateB = b.date.seconds;
+	// Set total expense when all expenses have been changed
+	useEffect(() => {
+		if (allExpenses) {
+			setTotalExpense(allExpenses);
+		}
+	}, [allExpenses]);
+	// Set total income when all incomes have been changed
+	useEffect(() => {
+		if (allIncomes) {
+			setTotalIncome(allIncomes);
+		}
+	}, [allIncomes]);
 
-      //     // Subtract the dates to get a value that is either negative, positive, or zero
-      //     return dateB - dateA;
-      //   })
-      // );
-    }
-  }, [allExpenses]);
-  // Set total income when all incomes have been changed
-  useEffect(() => {
-    if (allIncomes) {
-      setTotalIncome(allIncomes);
-      // setSortDateIncomes(
-      //   allIncomes.sort(function (a, b) {
-      //     // Convert the date strings to Date objects
-      //     let dateA = a.date.seconds;
-      //     let dateB = b.date.seconds;
+	useEffect(() => {
+		if (allIncomes && allExpenses) {
+			setSortDateExpensesAndIncome(
+				[...allIncomes, ...allExpenses].sort(function (a, b) {
+					// Convert the date strings to Date objects
+					let dateA = a.date.seconds;
+					let dateB = b.date.seconds;
 
-      //     // Subtract the dates to get a value that is either negative, positive, or zero
-      //     return dateB - dateA;
-      //   })
-      // );
-    }
-  }, [allIncomes]);
+					// Subtract the dates to get a value that is either negative, positive, or zero
+					return dateB - dateA;
+				}),
+			);
+		}
+	}, [allIncomes, allExpenses]);
+	// Set total if income or expense have been changed
+	useEffect(() => {
+		setTotal(totalIncome - totalExpense);
+	}, [totalExpense, totalIncome]);
 
-  useEffect(() => {
-    if (allIncomes && allExpenses) {
-      setSortDateExpensesAndIncome(
-        [...allIncomes, ...allExpenses].sort(function (a, b) {
-          // Convert the date strings to Date objects
-          let dateA = a.date.seconds;
-          let dateB = b.date.seconds;
+	// Render loading circle if haven't got data yet
+	if (isLoadingInWalletScreen) {
+		return (
+			<View className="flex-1 items-center justify-center">
+				<StatusBar backgroundColor={"#3a833c"} />
+				<ActivityIndicator
+					size={"large"}
+					color={"#4cb050"}
+				/>
+			</View>
+		);
+	}
 
-          // Subtract the dates to get a value that is either negative, positive, or zero
-          return dateB - dateA;
-        })
-      );
-    }
-  }, [allIncomes, allExpenses]);
-  // Set total if income or expense have been changed
-  useEffect(() => {
-    setTotal(totalIncome - totalExpense);
-  }, [totalExpense, totalIncome]);
+	return (
+		<View style={{ flex: 1 }}>
+			<StatusBar backgroundColor={"#3a833c"} />
 
-  // Render loading circle if haven't got data yet
-  if (isLoadingInWalletScreen) {
-    return (
-      <View className="flex-1 items-center justify-center">
-        <StatusBar backgroundColor={"#3a833c"} />
-        <ActivityIndicator size={"large"} color={"#4cb050"} />
-      </View>
-    );
-  }
+			{/* Header */}
+			<Header total={total} />
 
-  return (
-    <View style={{ flex: 1 }}>
-      <StatusBar backgroundColor={"#3a833c"} />
+			{/* Body */}
+			<View
+				className="mt-5 px-4"
+				style={{ flex: 1 }}>
+				{/* Overall section */}
+				<WhiteBox mt={"mt-4"}>
+					<Overall
+						totalExpense={totalExpense}
+						totalIncome={totalIncome}
+						total={total}
+					/>
+				</WhiteBox>
 
-      {/* Header */}
-      <Header total={total} />
+				{/* Each day section */}
+				{renderList ? (
+					<View
+						className="items-center justify-center flex-1"
+						style={{ marginTop: 40, marginBottom: 20 }}>
+						{/* All transactions show btn */}
+						<TouchableOpacity
+							className="flex-row items-end gap-1"
+							onPress={handleListVisible}>
+							<Text className="text-lg text-primary font-normal">
+								All transactions
+							</Text>
+							<Ionicons
+								name={chevronType}
+								size={20}
+								color={"#4cb050"}
+							/>
+						</TouchableOpacity>
 
-      {/* Body */}
-      <View className="mt-5 px-4" style={{ flex: 1 }}>
-        {/* Overall section */}
-        <WhiteBox mt={"mt-4"}>
-          <Overall
-            totalExpense={totalExpense}
-            totalIncome={totalIncome}
-            total={total}
-          />
-        </WhiteBox>
+						{/* Specific date show */}
+						<Animated.View
+							style={animatedStyle}
+							className="w-full mb-4">
+							<FlatList
+								showsVerticalScrollIndicator={false}
+								data={renderList}
+								extraData={renderList}
+								keyExtractor={(item) => item.title}
+								renderItem={({ item }) => (
+									<WhiteBox mt={"mt-4"}>
+										<DayOverall
+											inputDate={item.title}
+											transactions={item.data}
+										/>
+										{item.data.map((transaction) => (
+											<Transaction
+												key={transaction.id}
+												transaction={transaction}
+											/>
+										))}
+									</WhiteBox>
+								)}
+							/>
+						</Animated.View>
+					</View>
+				) : (
+					<View className="flex-1 justify-center items-center">
+						<Text className="text-lg">
+							You have not created any transaction yet
+						</Text>
+					</View>
+				)}
+			</View>
 
-        {/* Each day section */}
-        {renderList ? (
-          <View
-            className="items-center justify-center flex-1"
-            style={{ marginTop: 40, marginBottom: 20 }}
-          >
-            {/* All transactions show btn */}
-            <TouchableOpacity
-              className="flex-row items-end gap-1"
-              onPress={handleListVisible}
-            >
-              <Text className="text-lg text-primary font-normal">
-                All transactions
-              </Text>
-              <Ionicons name={chevronType} size={20} color={"#4cb050"} />
-            </TouchableOpacity>
-
-            {/* Specific date show */}
-            <Animated.View style={animatedStyle} className="w-full mb-4">
-              <FlatList
-                showsVerticalScrollIndicator={false}
-                data={renderList}
-                extraData={renderList}
-                keyExtractor={(item) => item.title}
-                renderItem={({ item }) => (
-                  <WhiteBox mt={"mt-4"}>
-                    <DayOverall
-                      inputDate={item.title}
-                      transactions={item.data}
-                    />
-                    {item.data.map((transaction) => (
-                      <Transaction
-                        key={transaction.id}
-                        transaction={transaction}
-                      />
-                    ))}
-                  </WhiteBox>
-                )}
-              />
-            </Animated.View>
-          </View>
-        ) : (
-          <View className="flex-1 justify-center items-center">
-            <Text className="text-lg">
-              You have not created any transaction yet
-            </Text>
-          </View>
-        )}
-      </View>
-
-      <EditModal />
-    </View>
-  );
+			<EditModal />
+		</View>
+	);
 }
